59368687c97e240db03bc971c40120acc8e43923
Todd Sparks <todd@drycanyon.com>
2016-12-01 16:20:05 -0700

    working

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -2,9 +2,13 @@
 
 var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only parameter.
+
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
-  //Code Here
+function isTyler(name) {
+  if(isTyler === name);
+    return true
+}
 
 //////////////////PROBLEM 2////////////////////
 
@@ -12,7 +16,9 @@ var name = 'Tyler';
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
 
-  //Code Here
+function getName() {
+  prompt()
+}
 
 
 //////////////////PROBLEM 3////////////////////
@@ -53,7 +59,7 @@ var name = 'Tyler';
 //Create a function called myName that returns your name
 
   //Code Here
-  
+
 
 
 //Now save the function definition of myName into a new variable called newMyName
